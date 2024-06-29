20baad88bca532a7fd3ddbab2a26d218da8c2b00
Yevhenii Baranik <baranik18@gmail.com>
2024-06-29 22:27:33 +0100

    init

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	const mobMenu = document.querySelector('.mob-menu');
 	const headerLinks = document.querySelectorAll('.header-item-link, .mob-item-link');
 
-	// Функция для открытия меню
 	function openMenu() {
 		mobMenu.style.visibility = 'visible';
 		mobMenu.style.opacity = '1';
@@ -12,7 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
 		document.body.classList.add('no-scroll');
 	}
 
-	// Функция для закрытия меню
 	function closeMenu() {
 		mobMenu.style.visibility = 'hidden';
 		mobMenu.style.opacity = '0';
@@ -23,23 +21,19 @@ document.addEventListener("DOMContentLoaded", function () {
 	openMenuButton.addEventListener('click', openMenu);
 	closeMenuButton.addEventListener('click', closeMenu);
 
-	// Закрытие меню при клике вне его области
 	document.addEventListener('click', function (event) {
 		if (!mobMenu.contains(event.target) && !openMenuButton.contains(event.target)) {
 			closeMenu();
 		}
 	});
 
-	// Обработчик кликов по ссылкам меню
 	headerLinks.forEach(link => {
 		link.addEventListener('click', function (event) {
-			// Удаляем класс 'active' у всех ссылок
+
 			headerLinks.forEach(link => link.classList.remove('active'));
 
-			// Добавляем класс 'active' к нажатой ссылке
 			this.classList.add('active');
 
-			// Плавное скроллирование к якорной ссылке
 			const targetId = this.getAttribute('href');
 			if (targetId.startsWith('#')) {
 				event.preventDefault();
